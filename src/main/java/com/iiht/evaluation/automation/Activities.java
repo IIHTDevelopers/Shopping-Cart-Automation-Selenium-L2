019e3d0f5a26ae7d565552af215c98480a099626
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-02-26 22:26:02 +0530

    Add files via upload

---
+++ src/main/java/com/iiht/evaluation/automation/Activities.java
@@ -0,0 +1,120 @@
+package com.iiht.evaluation.automation;
+
+import org.openqa.selenium.WebDriver;
+
+public class Activities {
+
+    public static boolean click_shop_menu(WebDriver driver) {
+        return false;
+    }
+    
+    public static boolean click_cart_icon(WebDriver driver) {
+        return false;
+    }
+
+    public static boolean add_current_product_count_in_excel(WebDriver driver) {
+        return false;
+    }
+
+    public static boolean click_home_menu_button(WebDriver driver) {
+        return false;
+    }
+    public static boolean check_home_page_has_three_arrivals(WebDriver driver) {
+        return false;
+    }
+    public static boolean click_product_product_image_in_arrival(WebDriver driver, String product_name) {
+        return false;
+    }
+    
+    public static boolean click_add_to_basket_button_product_detail_page(WebDriver driver) {
+        return false;
+    }
+    public static boolean check_product_added_message_product_detail_page(WebDriver driver, String product_name) {
+        return false;
+    }
+    public static int get_number_of_item_in_cart(WebDriver driver){
+        return -1;
+    }
+
+    public static boolean add_newly_added_product_count_in_excel(WebDriver driver) {
+        return false;
+    }
+
+    public static boolean check_newly_added_product_count_is_increment_in_excel(WebDriver driver) {
+        return false;
+    }
+
+    public static boolean check_apply_coupon_button(WebDriver driver) {
+        return false;
+    }
+    public static boolean click_item_detail_menu(WebDriver driver){
+        return false;
+    }
+
+    public static boolean check_if_price_defined_after_product_name(WebDriver driver){
+        return false;
+    }
+
+    public static boolean check_if_price_defined_in_correct_format(WebDriver driver){
+        return false;
+    }
+
+    public static boolean check_if_quantity_defined_after_product_price(WebDriver driver){
+        return false;
+    }
+
+    public static boolean check_if_quantity_defined_in_correct_format(WebDriver driver){
+        return false;
+    }
+
+    public static boolean check_if_total_defined_after_quantity(WebDriver driver){
+        return false;
+    }
+
+    public static boolean check_if_total_defined_in_correct_format(WebDriver driver){
+        return false;
+    }
+
+    public static boolean check_tax_defined_in_cart_page(WebDriver driver){
+        return false;
+    }
+    
+    public static boolean check_tax_defined_in_correct_format(WebDriver driver){
+        return false;
+    }
+
+    public static boolean check_if_remove_option_available(WebDriver driver){
+        return false;
+    }
+
+    public static boolean remove_product(WebDriver driver){
+        return false;
+    }
+
+    public static boolean check_message_after_removal_of_item(WebDriver driver){
+        return false;
+    }
+
+    public static boolean update_quantity(WebDriver driver){
+        return false;
+    }
+
+    public static boolean check_if_update_basket_button_available(WebDriver driver){
+        return false;
+    }
+
+    public static boolean check_checkout_button(WebDriver driver){
+        return false;
+    }
+
+    public static boolean click_checkout_button(WebDriver driver){
+        return false;
+    }
+
+    public static boolean check_navigation_to_billing_details_page(WebDriver driver){
+        return false;
+    }
+
+}
+
+
---
+++ src/test/java/com/iiht/evaluation/automation/functional/TestAutomation.java
@@ -0,0 +1,367 @@
+package com.iiht.evaluation.automation.functional;
+
+
+import static com.iiht.evaluation.automation.testutils.TestUtils.businessTestFile;
+import static com.iiht.evaluation.automation.testutils.TestUtils.currentTest;
+import static com.iiht.evaluation.automation.testutils.TestUtils.yakshaAssert;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+
+
+import com.iiht.evaluation.automation.Activities;
+import com.iiht.evaluation.automation.App;
+import com.iiht.evaluation.automation.testutils.MasterData;
+
+@TestMethodOrder(OrderAnnotation.class)
+public class TestAutomation extends App {
+
+    private static WebDriver driver;
+	private static Actions actions;
+	
+	
+	
+	public static String getHrefOfLink(WebElement w)
+	{
+		
+		if (w.getAttribute("outerHTML").startsWith("<a")) {
+			return w.getAttribute("href");
+		}
+		else
+			return w.getAttribute("outerHTML");
+		
+	}
+
+    @Test
+    @Order(1)
+    public void test_click_on_shop_menu() throws IOException {
+        boolean testcase_status = true;
+        try {
+            boolean click_shop_menu_succeed = Activities.click_shop_menu(driver);
+            System.out.println("click_shop_menu_succeed " + click_shop_menu_succeed);
+            if (!click_shop_menu_succeed) {
+                testcase_status = false;
+            }
+            System.out.println("testcase_status " + testcase_status);
+            yakshaAssert(currentTest(), testcase_status, businessTestFile);
+        } catch (Exception ex) {
+            System.out.println("ex " + ex);
+           yakshaAssert(currentTest(), testcase_status, businessTestFile);
+        }
+    }
+    @Test
+    @Order(2)
+    public void test_click_cart_icon() throws IOException {
+        boolean testcase_status = true;
+        try {
+            boolean click_cart_item_succeed = Activities.click_cart_icon(driver);
+           
+            if (!click_cart_item_succeed) {
+                testcase_status = false;
+            }
+            System.out.println("testcase_status " + testcase_status);
+            yakshaAssert(currentTest(), testcase_status, businessTestFile);
+        } catch (Exception ex) {
+            System.out.println("ex " + ex);
+           yakshaAssert(currentTest(), testcase_status, businessTestFile);
+        }
+    }
+
+    @Test
+    @Order(3)
+    public void test_add_current_product_count_in_excel() throws IOException {
+        boolean testcase_status = true;
+        try {
+            boolean add_current_product_count_in_excel = Activities.add_current_product_count_in_excel(driver);
+           
+            if (!add_current_product_count_in_excel) {
+                testcase_status = false;
+            }
+           // System.out.println("testcase_status " + testcase_status);
+            yakshaAssert(currentTest(), testcase_status, businessTestFile);
+        } catch (Exception ex) {
+            System.out.println("ex " + ex);
+           yakshaAssert(currentTest(), testcase_status, businessTestFile);
+        }
+    }
+
+
+
+    @Test
+    @Order(4)
+    public void test_arrival_images_add_to_basket_clik_home_menu_button() throws IOException {
+        boolean testcase_status = true;
+        try {
+            boolean click_home_menu_button_succeed = Activities.click_home_menu_button(driver);
+            System.out.println("click_home_menu_button_succeed " + click_home_menu_button_succeed);
+            if (!click_home_menu_button_succeed) {
+                testcase_status = false;
+            }
+            System.out.println("testcase_status " + testcase_status);
+            yakshaAssert(currentTest(), testcase_status, businessTestFile);
+        } catch (Exception ex) {
+            System.out.println("ex " + ex);
+            yakshaAssert(currentTest(), testcase_status, businessTestFile);
+        }
+    }
+
+  
+
+    @Test
+    @Order(5)
+    public void test_click_image_in_arrival() throws IOException {
+        boolean testcase_status = true;
+        String product_name = MasterData.test_arrival_add_to_basket_items_master_data.get("product_name");
+        try {
+            boolean click_image_in_arrival_succeed = Activities.click_product_product_image_in_arrival(driver,product_name);
+            System.out.println("click_image_in_arrival_succeed " + click_image_in_arrival_succeed);
+            if (!click_image_in_arrival_succeed) {
+                testcase_status = false;
+            }
+            System.out.println("testcase_status " + testcase_status);
+            yakshaAssert(currentTest(), testcase_status, businessTestFile);
+        } catch (Exception ex) {
+            System.out.println("ex " + ex);
+            yakshaAssert(currentTest(), testcase_status, businessTestFile);
+        }
+    }
+
+    @Test
+    @Order(6)
+    public void test_click_add_to_basket_button_product_detail_page() throws IOException {
+        boolean testcase_status = true;
+        try {
+            boolean click_add_to_basket_button_product_detail_page_succeed = Activities.click_add_to_basket_button_product_detail_page(driver);
+            System.out.println("click_add_to_basket_button_product_detail_page_succeed " + click_add_to_basket_button_product_detail_page_succeed);
+            if (!click_add_to_basket_button_product_detail_page_succeed) {
+                testcase_status = false;
+            }
+            System.out.println("testcase_status " + testcase_status);
+            yakshaAssert(currentTest(), testcase_status, businessTestFile);
+        } catch (Exception ex) {
+            System.out.println("ex " + ex);
+            yakshaAssert(currentTest(), testcase_status, businessTestFile);
+        }
+    }
+
+    @Test
+    @Order(7)
+    public void test_check_product_added_message_product_detail_page() throws IOException {
+        String product_name = MasterData.test_arrival_add_to_basket_items_master_data.get("product_name");
+        boolean testcase_status = true;
+        try {
+            boolean check_product_added_message_product_detail_page_succeed = Activities.check_product_added_message_product_detail_page(driver,product_name);
+            System.out.println("check_product_added_message_product_detail_page_succeed " + check_product_added_message_product_detail_page_succeed);
+            if (!check_product_added_message_product_detail_page_succeed) {
+                testcase_status = false;
+            }
+            System.out.println("testcase_status " + testcase_status);
+            yakshaAssert(currentTest(), testcase_status, businessTestFile);
+        } catch (Exception ex) {
+            System.out.println("ex " + ex);
+            yakshaAssert(currentTest(), testcase_status, businessTestFile);
+        }
+    }
+
+    @Test
+    @Order(8)
+    public void test_check_apply_coupon_button() throws IOException {
+        boolean testcase_status = true;
+        try {
+            boolean check_apply_coupon_button_succeed = Activities.check_apply_coupon_button(driver);
+           // System.out.println("click_item_detail_menu_succeed " + click_item_detail_menu_succeed);
+            if (!check_apply_coupon_button_succeed) {
+                testcase_status = false;
+            }
+            System.out.println("testcase_status " + testcase_status);
+            yakshaAssert(currentTest(), testcase_status, businessTestFile);
+        } catch (Exception ex) {
+            System.out.println("ex " + ex);
+            yakshaAssert(currentTest(), testcase_status, businessTestFile);
+        }
+    }
+
+    @Test
+    @Order(9)
+    public void test_click_item_detail_menu() throws IOException {
+        boolean testcase_status = true;
+        try {
+            boolean click_item_detail_menu_succeed = Activities.click_item_detail_menu(driver);
+           // System.out.println("click_item_detail_menu_succeed " + click_item_detail_menu_succeed);
+            if (!click_item_detail_menu_succeed) {
+                testcase_status = false;
+            }
+            System.out.println("testcase_status " + testcase_status);
+            yakshaAssert(currentTest(), testcase_status, businessTestFile);
+        } catch (Exception ex) {
+            System.out.println("ex " + ex);
+            yakshaAssert(currentTest(), testcase_status, businessTestFile);
+        }
+    }
+
+    @Test
+    @Order(10)
+    public void test_check_if_remove_option_available() throws IOException {
+        
+        boolean testcase_status = true;
+        try {
+            boolean check_if_remove_option_available_succeed = Activities.check_if_remove_option_available(driver);
+            //System.out.println("check_product_total_checkout_page_succeed " + check_product_total_checkout_page_succeed);
+            if (!check_if_remove_option_available_succeed) {
+                testcase_status = false;
+            }
+            System.out.println("testcase_status " + testcase_status);
+            yakshaAssert(currentTest(), testcase_status, businessTestFile);
+        } catch (Exception ex) {
+            System.out.println("ex " + ex);
+            yakshaAssert(currentTest(), testcase_status, businessTestFile);
+        }
+    }
+
+    @Test
+    @Order(11)
+    public void test_remove_product() throws IOException {
+        
+        boolean testcase_status = true;
+        try {
+            boolean remove_product_succeed = Activities.remove_product(driver);
+            //System.out.println("check_product_total_checkout_page_succeed " + check_product_total_checkout_page_succeed);
+            if (!remove_product_succeed) {
+                testcase_status = false;
+            }
+            System.out.println("testcase_status " + testcase_status);
+            yakshaAssert(currentTest(), testcase_status, businessTestFile);
+        } catch (Exception ex) {
+            System.out.println("ex " + ex);
+            yakshaAssert(currentTest(), testcase_status, businessTestFile);
+        }
+    }
+
+
+    @Test
+    @Order(12)
+    public void test_check_message_after_removal_of_item() throws IOException {
+        
+        boolean testcase_status = true;
+        try {
+            boolean check_message_after_removal_of_item_succeed = Activities.check_message_after_removal_of_item(driver);
+            //System.out.println("check_product_total_checkout_page_succeed " + check_product_total_checkout_page_succeed);
+            if (!check_message_after_removal_of_item_succeed) {
+                testcase_status = false;
+            }
+            System.out.println("testcase_status " + testcase_status);
+            yakshaAssert(currentTest(), testcase_status, businessTestFile);
+        } catch (Exception ex) {
+            System.out.println("ex " + ex);
+            yakshaAssert(currentTest(), testcase_status, businessTestFile);
+        }
+    }
+
+    @Test
+    @Order(13)
+    public void test_update_quantity() throws IOException {
+        
+        boolean testcase_status = true;
+        try {
+            boolean update_quantity_succeed = Activities.update_quantity(driver);
+            //System.out.println("check_product_total_checkout_page_succeed " + check_product_total_checkout_page_succeed);
+            if (!update_quantity_succeed) {
+                testcase_status = false;
+            }
+            System.out.println("testcase_status " + testcase_status);
+            yakshaAssert(currentTest(), testcase_status, businessTestFile);
+        } catch (Exception ex) {
+            System.out.println("ex " + ex);
+            yakshaAssert(currentTest(), testcase_status, businessTestFile);
+        }
+    }
+
+    @Test
+    @Order(14)
+    public void test_check_if_update_basket_button_available() throws IOException {
+        
+        boolean testcase_status = true;
+        try {
+            boolean check_if_update_basket_button_available_succeed = Activities.check_if_update_basket_button_available(driver);
+            //System.out.println("check_product_total_checkout_page_succeed " + check_product_total_checkout_page_succeed);
+            if (!check_if_update_basket_button_available_succeed) {
+                testcase_status = false;
+            }
+            System.out.println("testcase_status " + testcase_status);
+            yakshaAssert(currentTest(), testcase_status, businessTestFile);
+        } catch (Exception ex) {
+            System.out.println("ex " + ex);
+            yakshaAssert(currentTest(), testcase_status, businessTestFile);
+        }
+    }
+
+    @Test
+    @Order(15)
+    public void test_check_checkout_button() throws IOException {
+        
+        boolean testcase_status = true;
+        try {
+            boolean check_product_total_checkout_page_succeed = Activities.check_checkout_button(driver);
+            //System.out.println("check_product_total_checkout_page_succeed " + check_product_total_checkout_page_succeed);
+            if (!check_product_total_checkout_page_succeed) {
+                testcase_status = false;
+            }
+            System.out.println("testcase_status " + testcase_status);
+            yakshaAssert(currentTest(), testcase_status, businessTestFile);
+        } catch (Exception ex) {
+            System.out.println("ex " + ex);
+            yakshaAssert(currentTest(), testcase_status, businessTestFile);
+        }
+    }
+
+    @Test
+    @Order(16)
+    public void test_click_checkout_button() throws IOException {
+        
+        boolean testcase_status = true;
+        try {
+            boolean click_checkout_button_succeed = Activities.click_checkout_button(driver);
+            //System.out.println("check_product_total_checkout_page_succeed " + check_product_total_checkout_page_succeed);
+            if (!click_checkout_button_succeed) {
+                testcase_status = false;
+            }
+            System.out.println("testcase_status " + testcase_status);
+            yakshaAssert(currentTest(), testcase_status, businessTestFile);
+        } catch (Exception ex) {
+            System.out.println("ex " + ex);
+            yakshaAssert(currentTest(), testcase_status, businessTestFile);
+        }
+    }
+
+    @Test
+    @Order(17)
+    public void test_check_navigation_to_billing_details_page() throws IOException {
+        
+        boolean testcase_status = true;
+        try {
+            boolean check_navigation_to_billing_details_page_succeed = Activities.check_navigation_to_billing_details_page(driver);
+            //System.out.println("check_product_total_checkout_page_succeed " + check_product_total_checkout_page_succeed);
+            if (!check_navigation_to_billing_details_page_succeed) {
+                testcase_status = false;
+            }
+            System.out.println("testcase_status " + testcase_status);
+            yakshaAssert(currentTest(), testcase_status, businessTestFile);
+        } catch (Exception ex) {
+            System.out.println("ex " + ex);
+            yakshaAssert(currentTest(), testcase_status, businessTestFile);
+        }
+    }
+
+}
+
